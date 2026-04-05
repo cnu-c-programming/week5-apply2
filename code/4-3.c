b868141648e69300c5955617d502ebdf51283dfb
apply2 <67138665+apply2@users.noreply.github.com>
2026-04-06 00:17:43 +0900

    Update 4-3.c

---
+++ code/4-3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int size = 5;
+    int index;
+
+    scanf("%d", &index);
+
+    if (index < 5 && index >= 0)
+    {
+        printf("%d\n", arr[index]);
+    }
+    else {
+        printf("Error!\n");
+    }
+
+    return 0;
+} 
